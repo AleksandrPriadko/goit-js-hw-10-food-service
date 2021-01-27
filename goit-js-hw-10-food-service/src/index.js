a74ec420d260c38ecfe202893e50bc454fa0e850
AleksandrPriadko <68820567+AleksandrPriadko@users.noreply.github.com>
2021-01-27 23:43:05 +0200

    add markup

--- goit-js-hw-10-food-service/src/index.js
+++ goit-js-hw-10-food-service/src/index.js
@@ -1,9 +1,16 @@
 import './styles.css';
 import itemsTemplate from './templates/menu-items.hbs';
-import menu from './menu.json';
+import menuItem from './menu.json';
 
-const markup = itemsTemplate(menu);
+const markup = itemsTemplate(menuItem);
 console.log(markup);
 
 const template = document.querySelector('.js-menu');
 template.insertAdjacentHTML('beforeend', markup);
+
+/* const classMenuItem = document.querySelector("ul li");
+console.log(classMenuItem)
+classMenuItem.setAttribute("class", "menu__item");
+const addClassCard = document.querySelector("article");
+console.log(addClassCard)
+addClassCard.classList.add("card"); */
